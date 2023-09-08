ce6b3a2861ad2c477d079fd07935ff844824c4fd
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-08 12:53:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required
+var cnfrm = confirm("Do you want to proceed?");
+if(cnfrm == true) {
+	alert("You clicked OK. Proceeding...");
+	}
+else {
+	alert("You clicked Cancel. Exiting...");
+}
+
+
